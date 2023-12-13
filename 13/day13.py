59f98ddf5f7d099cb19e501f480603e7cc12758b
cameronb123 <cameronbarkerqwerty@gmail.com>
2023-12-13 09:47:41 +0000

    Day 13 part 2

--- 13/day13.py
+++ 13/day13.py
@@ -1,21 +1,50 @@
+import copy
 from pathlib import Path
+from typing import Optional
 
 
-def get_reflection_index(pattern: list[str]) -> int:
+def get_reflection_index(pattern: list[str]) -> list[int]:
     pattern_length = len(pattern)
+    reflection_indices = []
     for i in range(pattern_length - 1):
-        if all(pattern[i - k] == pattern[i + (k + 1)] for k in range(min(i + 1, pattern_length - i - 1))):
-            # print(i, pattern[i])
-            return 100 * (i + 1)
+        if all(
+            pattern[i - k] == pattern[i + (k + 1)]
+            for k in range(min(i + 1, pattern_length - i - 1))
+        ):
+            # Reflection is at the next index
+            reflection_indices.append(100 * (i + 1))
     row_length = len(pattern[0])
     for j in range(row_length - 1):
-        if all(row[j - k] == row[j + (k + 1)] for row in pattern for k in range(min(j + 1, row_length - j - 1))):
+        if all(
+            row[j - k] == row[j + (k + 1)]
+            for row in pattern
+            for k in range(min(j + 1, row_length - j - 1))
+        ):
             # Reflection is at the next index
-            return j + 1
-    return 0
+            reflection_indices.append(j + 1)
+    return reflection_indices
 
 
-def solution(file: Path) -> tuple[int, int]:
+def get_modified_reflection_index(
+    pattern: list[str], reflection_index: int
+) -> list[int]:
+    switch = {"#": ".", ".": "#"}
+    for i in range(len(pattern)):
+        for j in range(len(pattern[0])):
+            pattern_copy = pattern.copy()
+            pattern_copy[i] = (
+                pattern_copy[i][:j]
+                + switch[pattern_copy[i][j]]
+                + pattern_copy[i][j + 1 :]
+            )
+            copy_reflection_index = get_reflection_index(pattern_copy)
+            if reflection_index in copy_reflection_index:
+                copy_reflection_index.remove(reflection_index)
+            if copy_reflection_index:
+                return [copy_reflection_index[0]]
+
+
+def solution(file: Path, part2: bool = False) -> int:
     with open(file) as f:
         input_values = f.read().split("\n")
 
@@ -32,16 +61,25 @@ def solution(file: Path) -> tuple[int, int]:
 
     summary_total = 0
     for pattern in patterns:
-        summary_total += get_reflection_index(pattern)
-    return summary_total, 0
+        reflection_index = get_reflection_index(pattern)
+        if part2:
+            reflection_index = get_modified_reflection_index(
+                pattern, reflection_index[0]
+            )
+
+        summary_total += reflection_index[0]
+
+    return summary_total
 
 
 if __name__ == "__main__":
     test_file = Path(__file__).parent / "test.txt"
     input_file = Path(__file__).parent / "input.txt"
-    test_1, test_2 = solution(test_file)
+    test_1 = solution(test_file)
+    test_2 = solution(test_file, part2=True)
     assert test_1 == 405
-    assert test_2 == 0
-    part_1, part_2 = solution(input_file)
+    assert test_2 == 400
+    part_1 = solution(input_file)
+    part_2 = solution(input_file, part2=True)
     print("Part 1:", part_1)
     print("Part 2:", part_2)
